ed307c94fc501830d827e8692140a26cec86fa64
bienvenue cedrick <cedrick4smith@gmail.com>
2025-11-13 23:20:11 +0200

    docs: update Swagger configuration to include booking documentation

--- backend/src/swagger/index.ts
+++ backend/src/swagger/index.ts
@@ -2,9 +2,11 @@ import { swaggerConfig } from './config';
 import { authSchemas } from './schemas/authSchemas';
 import { routeSchemas } from './schemas/routeSchemas';
 import { scheduleSchemas } from './schemas/scheduleSchemas';
+import { bookingSchemas } from './schemas/bookingSchemas';
 import { authPaths } from './paths/authPaths';
 import { routePaths } from './paths/routePaths';
 import { schedulePaths } from './paths/schedulePaths';
+import { bookingPaths } from './paths/bookingPaths';
 
 export const swaggerDocument = {
   ...swaggerConfig,
@@ -12,6 +14,7 @@ export const swaggerDocument = {
     ...authPaths,
     ...routePaths,
     ...schedulePaths,
+    ...bookingPaths,
   },
   components: {
     ...swaggerConfig.components,
@@ -19,6 +22,7 @@ export const swaggerDocument = {
       ...authSchemas,
       ...routeSchemas,
       ...scheduleSchemas,
+      ...bookingSchemas,
     },
   },
 };
